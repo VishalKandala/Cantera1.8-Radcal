27e4daf5be9c42634766813ec873bfe719dace2a
vishalkandala <vishalkandala@tamu.edu>
2020-10-06 09:08:03 -0500

    First Commit

---
+++ Cantera/matlab/setup_matlab.py
@@ -0,0 +1,45 @@
+
+import sys
+
+######################################################################
+#  System Notes:
+#
+# HKM solaris 64:
+#   On this system matlab is built with gcc. Therefore, you will have to
+#   strip the "-lCrun -lCstd -lfsu" library commands from libs. They
+#   will cause a link error. because they are only applicable to
+#   sun's native CC compiler.
+#
+######################################################################
+
+bindir = '/usr/local/cantera/bin'
+libdir = '-L/home/wenjiang/Downloads/cantera-1.8-Duffour-test/build/lib/x86_64-unknown-linux-gnu  -L/home/wenjiang/Downloads/cantera-1.8-Duffour-test/build/lib/x86_64-unknown-linux-gnu'
+incdir = '/home/wenjiang/Downloads/cantera-1.8-Duffour-test/build/include'
+libs   = '-lclib  -luser -loneD -lzeroD -lequil -lkinetics -ltransport -lthermo -lctnumerics -lctmath -ltpx -lctspectra -lconverters -lctbase -lcvode -lctlapack -lctblas -lctf2c   -lctf2c -lm'
+
+f = open('setup.m','w')
+f.write('cd cantera\nbuildux\nexit\n')
+f.close()
+
+fb = open('cantera/buildux.m','w')
+fb.write("""
+disp('building Cantera..');
+mex -cxx -v private/ctmethods.cpp private/ctfunctions.cpp ...
+    private/xmlmethods.cpp private/phasemethods.cpp  ...
+    private/thermomethods.cpp private/kineticsmethods.cpp ...
+    private/mixturemethods.cpp ...
+    private/transportmethods.cpp private/reactormethods.cpp ...
+    private/reactornetmethods.cpp ...
+    private/wallmethods.cpp private/flowdevicemethods.cpp ...
+    private/funcmethods.cpp ...
+    private/onedimmethods.cpp private/surfmethods.cpp ...
+"""+'-I'+incdir+'     '+libdir+' '+libs+'\n'+"""disp('done.');
+""")
+fb.close()
+
+fp = open('cantera/ctbin.m','w')
+fp.write("""function path = ctbin
+path = '"""+bindir+"""';
+""")
+fp.close()
+
---
+++ Cantera/src/thermo/ThermoFactory.d
@@ -0,0 +1,14 @@
+ThermoFactory.o: ThermoFactory.cpp ThermoFactory.h ThermoPhase.h Phase.h \
+ State.h ../base/ct_defs.h ../base/config.h ../base/../../../config.h \
+ ../base/utilities.h ../base/ct_defs.h Constituents.h SpeciesThermo.h \
+ ../base/ctexceptions.h ../base/stringUtils.h ../base/xml.h \
+ ../base/ctexceptions.h ../base/global.h Elements.h \
+ ../base/vec_functions.h ../base/utilities.h ../base/ctml.h ../base/xml.h \
+ ../base/Array.h ../base/FactoryBase.h speciesThermoTypes.h \
+ ../base/global.h SpeciesThermoFactory.h IdealGasPhase.h mix_defs.h \
+ VPSSMgr.h VPSSMgrFactory.h IdealSolidSolnPhase.h PureFluidPhase.h \
+ ConstDensityThermo.h SurfPhase.h EdgePhase.h MetalPhase.h \
+ StoichSubstance.h MineralEQ3.h SingleSpeciesTP.h StoichSubstanceSSTP.h \
+ LatticeSolidPhase.h ../base/config.h LatticePhase.h HMWSoln.h \
+ MolalityVPSSTP.h VPStandardStateTP.h electrolytes.h DebyeHuckel.h \
+ ../base/Array.h IdealMolalSoln.h IdealSolnGasVPSS.h
